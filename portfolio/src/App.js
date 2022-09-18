ae0627da462bec1ea34a249d3d0e1c352d17702f
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-09-18 20:45:37 +0100

    IGNORED NODE

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Dev from "./components/Dev/Dev";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Records from "./components/Records/Records";
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Records />
       <Contact />
+      <Footer />
     </div>
   );
 }
--- portfolio/src/components/Records/Records.jsx
+++ portfolio/src/components/Records/Records.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Records.css"
 import { images } from '../../constants'
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 const Records = () => {
   return (
     <section class="records">
@@ -22,31 +24,50 @@ const Records = () => {
         <div class="container">
           <div class="wrap">
             <div class="record-circle">
-              <h2 class="number" data-num="235">0</h2>
+            <h2 class="number" >
+              <CountUp end={100} redraw={true}>
+                {({ countUpRef, start }) => (
+                    <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                    </VisibilitySensor>
+                )}
+             </CountUp>
+              </h2>
               <h4 class="sub-title">Projects</h4>
             </div>
           </div>
 
           <div class="wrap">
             <div class="record-circle active">
-              <h2 class="number" data-num="174">0</h2>
+            <h2 class="number" data-num="174">
+              <CountUp end={100} redraw={true}>
+                {({ countUpRef, start }) => (
+                    <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                    </VisibilitySensor>
+                )}
+             </CountUp>
+              </h2>
               <h4 class="sub-title">Happy Clients</h4>
             </div>
           </div>
 
           <div class="wrap">
             <div class="record-circle">
-              <h2 class="number" data-num="892">0</h2>
+            <h2 class="number" data-num="892">
+              <CountUp end={100} redraw={true}>
+                {({ countUpRef, start }) => (
+                    <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                    </VisibilitySensor>
+                )}
+             </CountUp>
+              </h2>
               <h4 class="sub-title">Work Hour</h4>
             </div>
           </div>
 
-          <div class="wrap">
-            <div class="record-circle">
-              <h2 class="number" data-num="368">0</h2>
-              <h4 class="sub-title">Awards</h4>
-            </div>
-          </div>
+          
         </div>
       </section>
   )
