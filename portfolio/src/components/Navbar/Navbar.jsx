2bd75b033654e19e3a954beca5af7265355c499f
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-09-19 11:28:11 +0100

    responsive hamburger done

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import "./Navbar.css"
+import "./Navbar.scss"
 import { images } from '../../constants'
+import { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav>
           <div className="container">
@@ -36,9 +41,35 @@ const Navbar = () => {
               </ul>
             </div>
 
-            <div className="hamburger-menu">
+            {/* <div className="hamburger-menu">
               <div className="bar"></div>
+            </div> */}
+
+
+
+            <div className="app__navbar-menu">
+              <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+              {toggle && (
+                <motion.div
+                  whileInView={{ x: [300, 0] }}
+                  transition={{ duration: 0.85, ease: 'easeOut' }}
+                >
+                  <HiX onClick={() => setToggle(false)} />
+                  <ul>
+                    {['home', 'services', 'portfolio', 'about', 'contact'].map((item) => (
+                      <li key={item}>
+                        <a href={`#${item}`} onClick={() => setToggle(false)}>
+                          {item}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </motion.div>
+              )}
             </div>
+
+
           </div>
         </nav>
   )
