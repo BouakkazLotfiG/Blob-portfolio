8a6aa18e0d5c323d15d9f4ac9c91f1b1cc12bba8
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-10-01 17:23:47 +0100

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,93 +7,30 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="grid-4">
-          <div className="grid-4-col footer-about">
-            <h3 className="title-sm">About</h3>
-            <p className="text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              officiis quo officia quia?
-            </p>
-          </div>
-
-          <div className="grid-4-col footer-links">
-            <h3 className="title-sm">Links</h3>
-            <ul>
-              <li>
-                <a href="#services">Services</a>
-              </li>
-              <li>
-                <a href="#portfolio">Portfolio</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#testimonials">Testimonials</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="grid-4-col footer-links">
-            <h3 className="title-sm">Services</h3>
-            <ul>
-              <li>
-                <a href="#">Web Design</a>
-              </li>
-              <li>
-                <a href="#">Web Dev</a>
-              </li>
-              <li>
-                <a href="#">App Design</a>
-              </li>
-              <li>
-                <a href="#">Marketing</a>
-              </li>
-              <li>
-                <a href="#">UI Design</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="grid-4-col footer-newstletter">
-            <h3 className="title-sm">Subscribe</h3>
-            <p className="text">
-              Lorem ipsum dolor, sit amet ipsum dolor sit amet.
-            </p>
-            <div className="footer-input-wrap">
-              <input type="email" className="footer-input" placeholder="Email" />
-              <a href="#" className="input-arrow">
-                <AiOutlineArrowUp />
-              </a>
-            </div>
-          </div>
-        </div>
-
         <div className="bottom-footer">
-          <div className="copyright">
-            <p className="text">
-             Made by <span>Lotfi</span>
-            </p>
-          </div>
-
-          <div className="followme-wrap">
-            <div className="followme">
-              <h3>Follow me</h3>
-              <span className="footer-line"></span>
-              <div className="social-media">
-                <a href="#">
-                  <FaInstagram />
-                </a>
-                <a href="#">
-                  <FaGithub />
-                </a>
-                <a href="#">
-                  <FaLinkedin />
-                </a>
-              </div>
+          <div className="followme-wrap-footer">
+            <div className="social-media-footer">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/lotfi__bkz/"
+              >
+                <FaInstagram size={30} />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/BouakkazLotfiG"
+              >
+                <FaGithub size={30} />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/ghanilotfibouakkaz/"
+              >
+                <FaLinkedin size={30} />
+              </a>
             </div>
 
             <div className="back-btn-wrap">
@@ -103,9 +40,14 @@ const Footer = () => {
             </div>
           </div>
         </div>
+        <div className="copyright">
+          <p className="text">
+            Made by <span>Lotfi</span>
+          </p>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
